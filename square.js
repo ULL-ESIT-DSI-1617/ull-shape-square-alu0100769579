9528fa0ef455f0f7aa2467d7e88208f5768c0a47
Raul Martin <alu0100769579@ull.edu.es>
2017-04-24 10:15:36 +0000

    Primer commit

---
+++ square.js
@@ -0,0 +1,11 @@
+let Shape = require('./shapesarea.js');
+class Square extends Shape {
+    constructor(options) {
+      super(options)
+    }
+    area() {
+      return  Math.pow(this.width,2) 
+    }
+}
+Shape.Shapes.Square = Square; 
+module.exports = Square;
