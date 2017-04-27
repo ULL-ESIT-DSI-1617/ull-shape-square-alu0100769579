ac9ad42ba8d0524574b67f30cea184013fe0dd85
Raul Martin <alu0100769579@ull.edu.es>
2017-04-27 18:05:32 +0000

    Añadiendo documentacion

--- square.js
+++ square.js
@@ -2,10 +2,34 @@
 
 let Shape = require('@alu0100769579/ull-shape-alu0100769579');
 
+/**
+ *Paquete npm de Square
+ * 
+ * Autor: Raúl Martín Morales
+ * 
+ * Aquí creamos la clase Square que hereda de Shape
+ * 
+ * @class Square
+ * @param {hash} options Ancho
+ * 
+ */
+ 
 class Square extends Shape {
+  
+    /**
+    * @construct Square
+    * @param{hash} options Ancho
+    */
+    
     constructor(options) {
       super(options)
     }
+    
+    /**
+    * @function getArea
+    * @return {value} retorna el valor del area del square
+    */
+    
     area() {
       return  Math.pow(this.width,2) 
     }
